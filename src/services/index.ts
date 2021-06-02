e0bdea5c97c3377cf84d8d1ed08634d816b0111c
Oscar Velandia <oscar.daniel.v.s.235@gmail.com>
2021-06-02 00:01:39 -0300

    Implement posts and comments API
    * Remove example API

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,1 +1,1 @@
-export * from './pokemonApi';
+export * from './postsAndCommentsApi';
---
+++ src/services/postsAndCommentsApi.ts
@@ -0,0 +1,33 @@
+import { createApi, fetchBaseQuery } from '@rtk-incubator/rtk-query/react';
+
+export interface PostComment {
+  postId: number;
+  id: number;
+  name: string;
+  email: `${string}@${string}.${string}`;
+  body: string;
+}
+
+export interface Post {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+}
+
+export const postsAndCommentsApi = createApi({
+  reducerPath: 'postsAndCommentsApi',
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'https://jsonplaceholder.typicode.com/',
+  }),
+  endpoints: (builder) => ({
+    getComments: builder.query<PostComment, void>({
+      query: () => `comments`,
+    }),
+    getPosts: builder.query<Post, void>({
+      query: () => `posts`,
+    }),
+  }),
+});
+
+export const { useGetCommentsQuery, useGetPostsQuery } = postsAndCommentsApi;
