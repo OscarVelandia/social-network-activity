2747d9ea8bed74623cf2a6be648ba1ca2747fca7
Oscar Velandia <oscar.daniel.v.s.235@gmail.com>
2021-06-05 14:05:50 -0300

    Add addNewComment reducer logic
    * Set one state update per reducer
    * Add initializeCommentsWithPostIdKey
    * Add new types

--- src/features/posts/postsSlice.ts
+++ src/features/posts/postsSlice.ts
@@ -1,55 +1,80 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { Post, PostComment } from '@services';
 
-interface AddCommentsToPostPayload {
+interface AddCommentsWithPostIdKeyPayload {
   comments: PostComment[];
+}
+
+interface AddCommentsToPostPayload {
   posts: Post[];
 }
+
+export type AddNewCommentPayload = Omit<PostComment, 'id'>;
 
 interface PostWithComments extends Post {
   comments: PostComment[];
 }
 
+interface CommentsWithPostIdKey {
+  [key: number]: PostComment[];
+}
+
 interface InitialState {
   postWithComments: PostWithComments[] | null;
+  commentsWithPostIdKey: CommentsWithPostIdKey | null;
 }
 
 const initialState: InitialState = {
   postWithComments: null,
+  commentsWithPostIdKey: null,
 };
 
 const postsSlice = createSlice({
   name: 'posts',
   initialState,
   reducers: {
-    addNewComment(state, action: PayloadAction<PostComment>) {
-      // state.postWithComments.map +=
-      console.log(action.payload);
+    addNewComment(state, action: PayloadAction<AddNewCommentPayload>) {
+      if (!state.commentsWithPostIdKey) {
+        return console.error('commentsWithPostIdKey state has no value.');
+      }
+
+      const { body, email, name, postId } = action.payload;
+      const newComment: PostComment = {
+        body,
+        email,
+        postId,
+        name,
+        id: state.commentsWithPostIdKey[postId].length + 1,
+      };
+
+      state.commentsWithPostIdKey[postId].unshift(newComment);
     },
     addCommentsToPost(state, action: PayloadAction<AddCommentsToPostPayload>) {
-      const { comments, posts } = action.payload;
-
-      const commentsWithPostIdKey = comments.reduce<{ [key: number]: PostComment[] }>(
-        (commentsWithKey, comment) => {
-          const { postId } = comment;
-
-          commentsWithKey[postId] =
-            commentsWithKey[postId]?.length > 0
-              ? commentsWithKey[postId].concat(comment)
-              : [comment];
-
-          return commentsWithKey;
-        },
-        {},
-      );
+      const { posts } = action.payload;
 
       state.postWithComments = posts.map((post) => ({
         ...post,
-        comments: commentsWithPostIdKey[post.id],
+        comments: state.commentsWithPostIdKey?.[post.id] || [],
       }));
+    },
+    initializeCommentsWithPostIdKey(state, action: PayloadAction<AddCommentsWithPostIdKeyPayload>) {
+      const commentsWithPostIdKey = action.payload.comments.reduce<{
+        // key is postId PostComment property
+        [key: number]: PostComment[];
+      }>((commentsWithKey, comment) => {
+        const { postId } = comment;
+
+        commentsWithKey[postId] =
+          commentsWithKey[postId]?.length > 0 ? commentsWithKey[postId].concat(comment) : [comment];
+
+        return commentsWithKey;
+      }, {});
+
+      state.commentsWithPostIdKey = commentsWithPostIdKey;
     },
   },
 });
 
-export const { addNewComment, addCommentsToPost } = postsSlice.actions;
+export const { addNewComment, addCommentsToPost, initializeCommentsWithPostIdKey } =
+  postsSlice.actions;
 export const postsReducer = postsSlice.reducer;
